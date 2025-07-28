49ae4d04e0c550d06b69ce6efb4a3fdae8e1c898
Pranay kumar <177r1a0574@gmail.com>
2025-07-28 16:28:32 +0530

    added resume to static files

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,6 @@ export const app = {
     linkedIn: 'https://linkedin.com/in/pranay-kumar-750288197/',
     github: 'http://github.com/variable6',
     email: `mailto:${email}`,
-    resume: 'https://docs.google.com/document/d/15dpUKucpxnALQGJtYYaU7upbtVqmQckjG5KDjOZkKws/edit?usp=sharing'
+    resume: '/resume.pdf'
   }
 }
